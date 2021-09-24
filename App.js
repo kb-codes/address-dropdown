693f1f0775dc141a61bbe952fc5ae32b1de5cdfe
Kuldip Badga <81810774+kb-codes@users.noreply.github.com>
2021-09-24 20:40:19 +0530

    static data added

--- App.js
+++ App.js
@@ -1,40 +1,82 @@
-import React, { useState, useEffect } from "react";
-import { View, StyleSheet, Dimensions } from "react-native";
-import DropDownPicker from "react-native-dropdown-picker";
-
-import country from "./constants/country.json";
+import React, { useState } from "react";
+import { View, StyleSheet, Dimensions, Text } from "react-native";
+import { Entypo } from "@expo/vector-icons";
+import ModalDropdown from "react-native-modal-dropdown";
+import country_data from "./constants/country.json";
+import state_data from "./constants/state.json";
+import city_data from "./constants/city.json";
 
 const width = Dimensions.get("window").width;
-export default function App() {
-  const [open, setOpen] = useState(false);
-  const [value, setValue] = useState(null);
-  const [items, setItems] = useState([
-    { label: "Apple", value: "apple" },
-    { label: "Banana", value: "banana" },
-  ]);
 
-  const [name, setName] = useState(
-    country.map((value) => {
+class DropDown extends React.Component {
+  constructor(props) {
+    super(props);
+  }
+
+  render() {
+    return (
+      <View
+        style={{
+          paddingVertical: 10,
+        }}
+      >
+        <ModalDropdown
+          options={this.props.value}
+          defaultValue={this.props.placeholder}
+          onSelect={(index, value) => {
+            console.log(value);
+          }}
+          style={{
+            borderWidth: 1,
+            padding: 10,
+            width: width - 30,
+          }}
+          textStyle={{
+            fontSize: 15,
+            color: "black",
+          }}
+          dropdownStyle={{
+            backgroundColor: "white",
+            width: width - 60,
+          }}
+          dropdownTextStyle={{
+            fontSize: 15,
+            color: "black",
+          }}
+          renderRightComponent={() => {
+            return <Entypo name="chevron-down" size={24} color="black" />;
+          }}
+          buttonAndRightComponentContainerStyle={{
+            justifyContent: "space-between",
+          }}
+        />
+      </View>
+    );
+  }
+}
+
+export default function App() {
+  const [country, setCountry] = useState(
+    country_data.map((value) => {
+      return value.name;
+    })
+  );
+  const [state, setState] = useState(
+    state_data.map((value) => {
+      return value.name;
+    })
+  );
+  const [city, setCity] = useState(
+    city_data.map((value) => {
       return value.name;
     })
   );
 
-  console.log(name);
-
   return (
     <View style={styles.container}>
-      <DropDownPicker
-        open={open}
-        value={value}
-        items={items}
-        setOpen={setOpen}
-        setValue={setValue}
-        setItems={setItems}
-        containerStyle={{
-          width: width - 50,
-        }}
-        placeholder="Select Country"
-      />
+      <DropDown placeholder="Select Country" value={country} />
+      <DropDown placeholder="Select State" value={state} />
+      <DropDown placeholder="Select City" value={city} />
     </View>
   );
 }
